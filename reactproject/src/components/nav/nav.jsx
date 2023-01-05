a2989bb850b2c790a0b5dd073b626bd39d554797
NEWHELL69 <kushagra0304@gmail.com>
2023-01-05 17:30:06 +0530

    Geist popover component removed(not accessible)

--- reactproject/src/components/nav/nav.jsx
+++ reactproject/src/components/nav/nav.jsx
@@ -1,16 +1,10 @@
 import { GeistProvider, CssBaseline } from '@geist-ui/core'
-import { Popover, Spacer} from '@geist-ui/core'
 import SearchBar from "../searchBar"
-import PopoverNav from './popoverNav'
 
 const Nav = () => {
-  const shopCategories = ["Men's clothing", "Jewelery", "Electronics", "Womes's clothing"]
-  const usCategories = ["Us"]
 
   return (
       <div className=''>
-        <PopoverNav categories={shopCategories} name="Menu" />
-        <PopoverNav categories={usCategories} name="Us" />
         <p>College Marketplace</p>
         <SearchBar/>
         <a href="#">Account</a>
