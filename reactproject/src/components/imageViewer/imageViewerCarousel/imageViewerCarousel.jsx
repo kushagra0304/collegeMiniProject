99d5fb299a70eeda09aae17de9f50091b3a5255a
NEWHELL69 <kushagra0304@gmail.com>
2023-01-22 20:25:31 +0530

    ImageViewerCarousel_22_01_23

--- reactproject/src/components/imageViewer/imageViewerCarousel/imageViewerCarousel.jsx
+++ reactproject/src/components/imageViewer/imageViewerCarousel/imageViewerCarousel.jsx
@@ -1,6 +1,48 @@
+import { useEffect } from "react";
 import "./imageViewerCarousel.css"
 
+
 const ImageViewerCarousel = ({ images }) => {
+
+    useEffect(() => {
+        const inner =  document.querySelector(".imageViewerCarousel .imageViewer__inner")
+        let img = document.querySelector(".imageViewerCarousel img");
+        const prevBtn = document.querySelector(".imageViewerCarousel .prev");
+        const nextBtn = document.querySelector(".imageViewerCarousel .next")
+
+        const observer = new IntersectionObserver(function(entries) {
+            for(let i = 0; i < entries.length; i++) {
+                if(entries[i].isIntersecting === true && (Math.round(entries[i].intersectionRatio * 10) / 10)== 0.9) {
+                    img = entries[i].target;
+                }
+            }
+
+        }, { threshold: [0.9], root: inner });
+        
+        let temp = img;
+        while(temp !== null){
+            observer.observe(temp);
+            temp = temp.nextElementSibling;
+        }
+
+        const prevImage = () => {
+            inner.scrollLeft = img.previousElementSibling.offsetLeft;
+        }
+
+        const nextImage = () => {
+            inner.scrollLeft = img.nextElementSibling.offsetLeft;
+        }
+
+        prevBtn.addEventListener("click", prevImage)
+        nextBtn.addEventListener("click", nextImage)
+
+        return () => {
+            prevBtn.removeEventListener("click", prevImage);
+            nextBtn.removeEventListener("click", nextImage);
+            observer.disconnect();
+        }
+    }, [])
+
     return (
         <>
             <div className="imageViewerCarousel">
@@ -15,6 +57,8 @@ const ImageViewerCarousel = ({ images }) => {
                         })
                     }
                 </div>
+                <button className="prev">{"<--"}</button>
+                <button className="next">{"-->"}</button>
             </div>
         </>
     )
