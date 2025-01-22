7480c61e55e822684e71604acd79d41059b93ddb
NEWHELL69 <kushagra0304@gmail.com>
2025-01-23 02:02:22 +0530

    build

---
+++ reactproject/build/static/js/870.e3014336.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreactproject=self.webpackChunkreactproject||[]).push([[870],{870:function(e,r,c){c.r(r);var t=c(184);r.default=function(){for(var e=0;e<1e9;e++);return(0,t.jsx)("h1",{children:"Hello"})}}}]);
+//# sourceMappingURL=870.e3014336.chunk.js.map
